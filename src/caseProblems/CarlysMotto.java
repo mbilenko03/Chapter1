eb7f8cd44cdecff5f4c61384d497dd32dabb6cce
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-08-30 09:17:28 -0500

    Added Case Problems

---
+++ src/caseProblems/CarlysMotto.java
@@ -0,0 +1,10 @@
+package caseProblems;
+
+public class CarlysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/caseProblems/SammysMotto.java
@@ -0,0 +1,10 @@
+package caseProblems;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
