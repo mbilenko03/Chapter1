f08f02405c51697f06ae6824f85e0a5b1aff693d
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-08-30 20:42:15 -0500

    Update

--- src/gameZone/RandomGuess.java
+++ src/gameZone/RandomGuess.java
@@ -7,7 +7,7 @@ public class RandomGuess {
 	public static void main(String[] args) 
 	{
 		JOptionPane.showMessageDialog(null, "Think of a number from 1 to 10.");
-		JOptionPane.showMessageDialog(null, "The nuber is " + (1+ (int)(Math.random() * 10)));
+		JOptionPane.showMessageDialog(null, "The number is " + (1+ (int)(Math.random() * 10)));
 	}
 
 }
